c105777f144b1c4dde6eb99780a278b51f523996
Tommy Chabiras <tommychabiras@gmail.com>
2025-09-01 13:31:36 -0400

    basic todo component

---
+++ src/lib/components/TodoItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { todo }: { todo: Todo } = $props();
+	let completed = $state(todo.completed);
+</script>
+
+<li>
+	<button onclick={() => (completed = !completed)}>
+		{#if completed}
+			✅
+		{:else}
+			⬜
+		{/if}
+	</button>
+	<p>{todo.title}</p>
+	<p>{todo.description}</p>
+	<button>expand button</button>
+</li>
